1b2ecfa34e714f8c998564129435889e8869f913
Michelle Tharp <mikitharp@gmail.com>
2021-06-23 18:02:32 +0000

    Working on Part 3 now

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,29 +8,47 @@ let candidateName;
 let question;
 let correctAnswer;
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let questions = ['Who was the first American woman in space?', 'True or false: 5 kilometer == 5000 meters?', '(5 + 3)/2 * 10 = ?','Given the array [8, \'Orbit\', \'Trajectory\', 45], what entry is at index 2?', 'What is the minimum crew size for the ISS?'];
+let correctAnswers = ["Sally Ride", "true", "40", "trajectory", "3"];
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+let info = input.question ("What is your name? ")
+  candidateName = info ;
+  console.log ("Welcome, " + info)
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  for (let i = 0; i < questions.length; i++){
+     const candidateInput = input.question (questions[i])
+    candidateAnswers.push(candidateInput)
+    
+  }
+  console.log(candidateAnswers)
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
+let correctNum = 0
 
-  let grade;
+for (i = 0; i < questions.length; i++){
+  console.log(candidateAnswers[i])
+  console.log(correctAnswers[i])
+  if (candidateAnswers[i].toLowerCase == correctAnswers[i].toLowerCase){
+    console.log("Correct!")
+    correctNum += 1 
   
+  }
+ 
+}
+console.log(correctNum)
+console.log(correctAnswers)
+  let grade = (correctNum) / (questions.length) * 100 ;
+  console.log(grade)
 
   return grade;
 }
@@ -42,6 +60,8 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
